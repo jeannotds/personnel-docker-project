405d93ac6ac423d32388dd171b9def52e1f922bb
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-18 15:26:13 +0100

    clean code

--- frontend/components/Home/Stat/Statistics.tsx
+++ frontend/components/Home/Stat/Statistics.tsx
@@ -6,7 +6,6 @@ interface StaticsProps {
 }
 
 const Statistics = ({ companies }: StaticsProps) => {
-  console.log("companies : ", companies);
   return (
     <div className="px-16 py-9 grid grid-cols-4 gap-4">
       <div className="flex flex-col gap-3 px-4 py-4 bg-white rounded-2xl border shadow-2xs border-[rgba(71,63,63,0.1)]">
