52364b9cd03014832627bb885f79fc9152d70e0e
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-18 14:00:13 +0100

    fix: navbar

--- frontend/components/Home/Navbar/Nav.tsx
+++ frontend/components/Home/Navbar/Nav.tsx
@@ -2,15 +2,14 @@ import { Bell } from "lucide-react";
 
 const Nav = () => {
   return (
-    // <div>
-    <div className="px-4 flex flex-row font-semibold fixed justify-between p-7 h-[10vh] bg-white01 w-full">
+    <div className="px-16 flex flex-row font-semibold justify-between p-7 h-[10vh] bg-white01 fixed right-0 left-[20%] z-10">
       <div className="flex flex-col">
         <h1 className="font-bold text-lg">Tableau de bord</h1>
         <div className="text-[12px] font-light opacity-60">
           Vue d'ensemble de votre organisation
         </div>
       </div>
-      <div className="flex flex-row items-center gap-6">
+      <div className="flex flex-row items-center gap-10">
         <div className="bg-[#ebebeb] py-[6] px-3 rounded-md border border-[#dcdcdc]">
           <input
             className=" placeholder:text-sm placeholder:text-[#2c2c2c] placeholder:opacity-70 placeholder:font-light outline-none focus:outline-none"
@@ -34,7 +33,6 @@ const Nav = () => {
         </div>
       </div>
     </div>
-    // </div>
   );
 };
 
