5b8bda16ab121641033497104b4d7567f1cea3f8
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-19 22:09:40 +0100

    add dockerignore

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,25 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
 
-
-app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    // disableErrorMessages: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }),
-);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // disableErrorMessages: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   app.enableCors({
-    origin: "http://localhost:3006"
+    // origin: "http://localhost:3006"
+    origin: '*',
   });
 
   await app.listen(process.env.PORT ?? 3005);
-
-
 }
 bootstrap();
