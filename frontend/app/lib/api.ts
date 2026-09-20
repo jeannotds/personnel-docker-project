59aeb5b791cde179416e54274193c5307e9aadb6
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-20 02:30:59 +0100

    migrate and build and up container

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    typeof window === "undefined"
+      ? process.env.INTERNAL_API_URL
+      : process.env.NEXT_PUBLIC_API_URL,
   timeout: 5000,
   headers: { "Content-Type": "application/json", "X-Custom-Header": "foobar" },
 });
