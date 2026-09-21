fc722bff9957b207836c99669f1fe544c0b7d56d
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-21 20:26:43 +0100

    config nginx

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,6 +19,18 @@ async function bootstrap() {
     origin: '*',
   });
 
-  await app.listen(process.env.PORT ?? 3005);
+  // await app.listen(process.env.PORT ?? 3005);
+
+  // config docker
+  await app.listen(process.env.PORT ? 3005 : '0.0.0.0');
 }
 bootstrap();
+
+// backend container
+// 127.0.0.1:3005
+//       ↑
+// accessible seulement depuis le backend lui-même ❌
+
+// 0.0.0.0:3005
+//       ↑
+// accessible depuis les autres conteneurs ✅
