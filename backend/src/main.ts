10735a8bd935d371371aca3ea4fac657ca03a2e5
Diambu Jeannot <diambujeannot@gmail.com>
2026-09-22 18:07:40 +0100

    improve compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   // await app.listen(process.env.PORT ?? 3005);
 
   // config docker
-  await app.listen(process.env.PORT ? 3005 : '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3005, '0.0.0.0');
 }
 bootstrap();
 
